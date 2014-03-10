b9faec6a5e307cd76adbb728449e66446fa77f14
elidarrow <elidarrow@gmail.com>
2014-03-10 09:40:48 -0700

    cleanup commit

--- public/trie.js
+++ public/trie.js
@@ -17,78 +17,68 @@ Trie.prototype.learn = function(word, index){
   // A word does not necessarily end at a leaf.
   // You must mark nodes which are the ends of words,
   // so that the words can be reconstructed later.
-if (!index) {
-    index = 0;
-}
-var letter = word[index];
-if(!letter) {
-  this.isWord = true;
-}
-  else if (this.characters[letter]) {
-  index++;
-  this.characters[letter].learn(word, index);
-}
-else {
-  this.characters[letter] = new Trie();
-  index++;
-  this.characters[letter].learn(word, index);
+  if (!index) {var index = 0;}
+  var letter = word[index];
+  if (letter === undefined){
+    this.isWord = true;
+  } else if ( this.characters[letter] ) {
+    index ++;
+    this.characters[letter].learn(word, index);
+  } else {
+    index ++;
+    this.characters[letter] = new Trie();
+    this.characters[letter].learn(word, index);
   }
 
 };
 
 Trie.prototype.getWords = function(words, currentWord){
-  // This function will return all the words which are
-  // contained in this Trie.
-  // it will use currentWord as a prefix,
-  // since a Trie doesn't know about its parents.
-  words = words || [];
-  currentWord = currentWord || "";
+ // This function will return all the words which are
+ // contained in this Trie.
+ // it will use currentWord as a prefix,
+ // since a Trie doesn't know about its parents.
 
-  if (this.isWord) {
-  words.push(currentWord);
-  }
+ words = words || [];
+ currentWord = currentWord || "";
 
-  for ( var letter in this.characters ) {
-    var newWord = currentWord + letter;
-    this.characters[letter].getWords(words, newWord);
-  }
-  return words;
+ if (this.isWord) {
+   words.push(currentWord);
+ }
+
+ for ( var letter in this.characters ) {
+   var newWord = currentWord + letter;
+   this.characters[letter].getWords(words, newWord);
+ }
+
+ return words;
+
 };
 
-
-Trie.prototype.find = function(word, index) {
-  // This function will return the node in the trie
-  // which corresponds to the end of the passed in word.
-
-  // Be sure to consider what happens if the word is not in this Trie.
-  index = index || 0;
-  word = word || "";
-
+Trie.prototype.find = function(word, index){
+  if (!index) { var index = 0;}
   var letter = word[index];
-  if (!letter) {
+  if (letter === undefined){
     return this;
-  } 
-  else if (!this.characters[letter]) {
-    return null;
-  } 
-  else {
-    index++;
+  } else if ( this.characters[letter] ) {
+    index ++;
     return this.characters[letter].find(word, index);
+  } else {
+    return false;
   }
+
 };
 
 Trie.prototype.autoComplete = function(prefix){
-  // This function will return all completions 
-  // for a given prefix.
-  // It should use find and getWords.
   var words = [];
-  prefix = prefix || "";
-  var suffixes = this.find(prefix).getWords();
-  if (!suffixes) {
-    return words;
+  if (this.find(prefix)){
+    var suffixes = this.find(prefix).getWords();
+
+    for (var i = 0; i < suffixes.length; ++i){
+      words.push( prefix + suffixes[i] );
+    }
+    
   }
-  for ( var i = 0; i < suffixes.length; i++ ) {
-    words.push(prefix + suffixes[i]);
-  }
+
   return words;
+
 };
